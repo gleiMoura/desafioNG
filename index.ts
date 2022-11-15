c8f6d402f1f546e1ef0118b6d0c2fa11d4977196
gleiMoura <gle.bar.mou@gmail.com>
2022-11-15 13:45:07 -0300

    First commit

---
+++ index.ts
@@ -0,0 +1,19 @@
+import express, { json } from "express";
+import cors from "cors";
+import chalk from "chalk";
+import dotenv from "dotenv";
+import "express-async-errors";
+import router from "./src/router/index.js";
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(json());
+app.use(router);
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+    console.log(chalk.green(`API is up in port ${port}`));
+})
