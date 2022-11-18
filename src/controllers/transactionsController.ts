90b95397857c537460ec9ec1bbeb06ca33fa804f
gleiMoura <gle.bar.mou@gmail.com>
2022-11-18 20:18:41 -0300

    put tow routers to show cash-in or cash-out transactions by date

--- src/controllers/transactionsController.ts
+++ src/controllers/transactionsController.ts
@@ -23,3 +23,24 @@ export async function getTransactionsByDate(req: Request, res: Response) {
 
 	res.status(200).send(transactions);
 };
+
+export async function getCashInByDate(req: Request, res: Response) {
+	const { authorization } = req.headers;
+	const userId = getUserIdByToken(authorization);
+	const { date }:{date:string} = req.body;
+
+	const transactions = await getTransactionsByDateAndUserId(userId, date);
+
+	res.status(200).send(transactions.creditedTransactions);
+};
+
+export async function getCashOutByDate(req: Request, res: Response) {
+	const { authorization } = req.headers;
+	const userId = getUserIdByToken(authorization);
+	const { date }:{date:string} = req.body;
+
+	const transactions = await getTransactionsByDateAndUserId(userId, date);
+
+	res.status(200).send(transactions.debitedTransactions);
+};
+
--- src/router/transactionsRouter.ts
+++ src/router/transactionsRouter.ts
@@ -1,15 +1,33 @@
 import router from "express";
 import transactionDateSchema from "../schemas/transactionDateSchema.js";
 import schemaValidator from "../middlewares/schemaValidator.js";
-import { getTransactions, getTransactionsByDate } from "../controllers/transactionsController.js";
+import {
+	getTransactions, 
+	getTransactionsByDate,
+	getCashInByDate,
+	getCashOutByDate
+} from "../controllers/transactionsController.js";
 
 const transactionsRouter = router();
 
-transactionsRouter.get("/getTransactions", getTransactions);
+transactionsRouter.get(
+	"/getTransactions", 
+	getTransactions
+);
 transactionsRouter.get(
 	"/getTransactions/byDate",
 	schemaValidator(transactionDateSchema), 
 	getTransactionsByDate
+);
+transactionsRouter.get(
+	"getTransactions/byDate/cashIn",
+	schemaValidator(transactionDateSchema),
+	getCashInByDate
+);
+transactionsRouter.get(
+	"getTransactions/byDate/cashOut",
+	schemaValidator(transactionDateSchema),
+	getCashOutByDate
 )
 
 export default transactionsRouter;
